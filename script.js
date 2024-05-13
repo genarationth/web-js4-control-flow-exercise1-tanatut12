8db20086eaa847752388641864666d0a8ee9d45a
tanatut12 <tanatut12@gmail.com>
2024-05-13 15:20:35 +0700

    finish

---
+++ script.js
@@ -0,0 +1,18 @@
+const dayNumber = 9;
+if(dayNumber === 0) {
+    console.log('sunday');
+} else if(dayNumber === 1) {
+    console.log('monday');
+} else if(dayNumber === 2) {
+    console.log('tuesday');
+} else if(dayNumber === 3) {
+    console.log('wednesday');
+} else if(dayNumber === 4) {
+    console.log('thursday');
+} else if(dayNumber === 5) {
+    console.log('friday');
+} else if(dayNumber === 6) {
+    console.log('saturday');
+} else {
+    console.log("Invalid day number");
+}
